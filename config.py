08c7059a22e40bb71e85cd92f00005bc28690553
Smith <baror@gmail.com>
2019-04-10 18:27:37 +0700

    add config

---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+base_dir = ''
+public_html = False
+included_domain = []
+excluded_domain = []
+mysql_host = 'localhost'
+mysql_user = ''
+mysql_pass = ''
+pass_to_scj = ''
+admin_email = ''
+prefix_to_folder = ''
+prefix_to_db = ''
